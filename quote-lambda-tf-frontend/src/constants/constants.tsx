7e523293e56700ae2fe84c3d7fd6274fd3eefd4f
edwinbulter <edwin.bulter@gmail.com>
2025-12-22 21:19:34 +0100

    [ECS-30] Fix frontend 403 caused by trailing slash added by api-gateway

--- quote-lambda-tf-frontend/src/constants/constants.tsx
+++ quote-lambda-tf-frontend/src/constants/constants.tsx
@@ -1,2 +1,2 @@
-export const BASE_URL = import.meta.env.VITE_REACT_APP_API_BASE_URL + "/api/v1";
+export const BASE_URL = import.meta.env.VITE_REACT_APP_API_BASE_URL.replace(/\/$/, '') + "/api/v1";
 export const SSE_URL = BASE_URL + "/quote/stream";
